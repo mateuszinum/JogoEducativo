971772525559fbbcea3d85c7420677d18b10f54a
Mateus A. Muniz S. <mateuszinum@gmail.com>
2025-11-19 11:25:58 -0400

    Update Movimentação do Inimigo
    Tentativa de otimizar navegação dos Inimigos
    
    Limite de Inimigos na Tela: Aproximadamente 200

--- Scripts/Enemy/enemy.gd
+++ Scripts/Enemy/enemy.gd
@@ -1,43 +1,58 @@
 extends CharacterBody2D
 
-@export var speed = 50
-@export var nav_agent : NavigationAgent2D
+@export var speed: float = 60.0
+@export var nav_agent: NavigationAgent2D
 
-var target_node = null
-var home_pos = Vector2.ZERO
+var use_navigation := false  # Troca entre modos de navegação
 
 
 func _ready():
-	home_pos = self.global_position
+	var player = get_node("/root/World/Player")
 	nav_agent.path_desired_distance = 4
 	nav_agent.target_desired_distance = 4
 
 
-func _physics_process(_delta: float) -> void:
-	if nav_agent.is_navigation_finished():
+func _physics_process(_delta):
+	var player: Node2D = get_node("/root/World/Player")
+
+	# Modo 1: Seguir Player
+	if not use_navigation:
+		var direction := global_position.direction_to(player.global_position)
+
+		# Atualiza Raycast para olhar para frente
+		$RayCast2D.target_position = direction * 64
+		$RayCast2D.force_raycast_update()
+
+		# Se encontra parede -> troca modo de navegação
+		if $RayCast2D.is_colliding():
+			use_navigation = true
+			nav_agent.target_position = player.global_position
+			return
+
+		# Move diretamente para o player
+		velocity = direction * speed
+		move_and_slide()
 		return
-		
-	var axis = to_local(nav_agent.get_next_path_position()).normalized()
-	velocity = axis * speed
-	
-	move_and_slide()
 
 
-func recalc_path():
-	if target_node:
-		nav_agent.target_position = target_node.global_position
-	else:
-		nav_agent.target_position = home_pos
+	# Modo 2: Navegar até o Player
+	if use_navigation:
+		# Sempre atualiza o target para ser o player
+		nav_agent.target_position = player.global_position
 
+		if nav_agent.is_navigation_finished():
+			# Quando navegação acaba -> muda de volta para Modo 1
+			use_navigation = false
+			return
 
-func _on_recalculate_timer_timeout() -> void:
-	recalc_path()
+		var next_pos = nav_agent.get_next_path_position()
+		var direction := (next_pos - global_position).normalized()
 
+		velocity = direction * speed
+		move_and_slide()
 
-func _on_aggro_range_area_entered(area: Area2D) -> void:
-	target_node = area.owner
-
-
-func _on_de_aggro_range_area_exited(area: Area2D) -> void:
-	if area.owner == target_node:
-		target_node = null
+		# Se RayCast não encontra nada, volta para Modo 1
+		$RayCast2D.target_position = direction * 64
+		$RayCast2D.force_raycast_update()
+		if not $RayCast2D.is_colliding():
+			use_navigation = false
