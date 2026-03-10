7d34112565e87677bc4661162e877df9c5a95d18
P. Vargas <pedrovargas.cba@gmail.com>
2026-03-10 17:23:39 -0400

    removido som de hit em inimigos

--- Scripts/Enemy/enemyType.gd
+++ Scripts/Enemy/enemyType.gd
@@ -4,13 +4,7 @@ class_name Enemy
 @export var title : String
 @export var animations : SpriteFrames 
 
-@export var health : float
-@export var damage : float
+@export var health : float = 3
+@export var damage : float = 10
 @export var speed : float = 10.0
 @export var despawns : bool = true
-
-@export_group("Audio")
-@export var hurt_sound : AudioStream
-@export var hurt_volume : float = 0.0
-@export var pitch_min : float = 0.8
-@export var pitch_max : float = 1.2
