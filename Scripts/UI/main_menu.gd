5ad8fac33dc8a1ff84b2e89bf8b377feda9b1fa2
Mateus A. Muniz S. <mateuszinum@gmail.com>
2026-03-15 15:00:00 -0400

    Cena do Vilarejo
    Protótipo adicionado de cena do vilarejo com Botão para iniciar partida

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -8,7 +8,7 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/World/world.tscn")
+	get_tree().change_scene_to_file("res://Scenes/UI/village_menu.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
