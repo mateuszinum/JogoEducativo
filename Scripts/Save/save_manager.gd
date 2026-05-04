252f33426b4198cba72dddc98937e30ea74d8d23
Mateus A. Muniz S. <mateuszinum@gmail.com>
2026-05-04 13:19:43 -0400

    Correcao save_manager e save_master adicionado

--- Scripts/Save/save_manager.gd
+++ Scripts/Save/save_manager.gd
@@ -1,58 +1,39 @@
 extends Node
 
-func obter_diretorio_save() -> String:
-	var caminho_completo = ""
-	var sistema = OS.get_name()
+# Nos métodos Get, trocar o void por Dictionary
+
+func SalvarSkillTree(dados: Dictionary) -> void:
+	pass
+
+func GetSkillTree() -> void:
+	pass
 	
-	if sistema == "Windows":
-		caminho_completo = OS.get_environment("USERPROFILE") + "/AppData/LocalLow/Grimorio"
-		
-	elif sistema == "Linux" or sistema == "FreeBSD":
-		caminho_completo = OS.get_environment("HOME") + "/.local/share/Grimorio"
-
-	if not DirAccess.dir_exists_absolute(caminho_completo):
-		pass
-		# cria pasta
-
-			
-	return caminho_completo
-
-
-func obter_caminho_slot(slot_id: int) -> String:
-	return obter_diretorio_save() + "/slot" + str(slot_id) + ".txt"
-
-
-func salvar_jogo(slot_id: int, dados_do_jogo: Dictionary):
-	var caminho = obter_caminho_slot(slot_id)
+func SalvarAtributosBruxa(dados: Dictionary) -> void:
+	pass
 	
-	# pra encriptar, usar o método .open_encrypted_with_pass
-	var arquivo = FileAccess.open(caminho, FileAccess.WRITE)
-	
-	if arquivo:
-		var json_string = JSON.stringify(dados_do_jogo, "\t")
-		
-		arquivo.store_string(json_string)
-		arquivo.close()
-		print("Salvo com sucesso no slot ", slot_id, " em: ", caminho)
-	else:
-		print("Erro ao criar o arquivo em: ", caminho)
+func GetAtributosBruxa() -> void:
+	pass
 
+func SalvarInventario(dados: Dictionary) -> void:
+	pass
 
-func carregar_jogo(slot_id: int) -> Dictionary:
-	var caminho = obter_caminho_slot(slot_id)
+func GetInventario() -> void:
+	pass
 	
-	if not FileAccess.file_exists(caminho):
-		return {}
-		
-	var arquivo = FileAccess.open(caminho, FileAccess.READ)
-	var json_string = arquivo.get_as_text()
-	arquivo.close()
+func SalvarRecursos(dados: Dictionary) -> void:
+	pass
 	
-	var json = JSON.new()
-	var erro = json.parse(json_string)
+func GetRecursos() -> void:
+	pass
+
+func SalvarCodigo(dados: Dictionary) -> void:
+	pass
 	
-	if !erro:
-		return json.data
-	else:
-		print("Erro de formatação no JSON do save: ", json.get_error_message())
-		return {}
+func GetCodigo() -> void:
+	pass
+
+func SalvarConfig(dados: Dictionary) -> void:
+	pass
+	
+func GetConfig() -> void:
+	pass
