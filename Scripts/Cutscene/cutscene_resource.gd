e28fa730dadb44f2c089ca7eec2224e30c853bbc
P. Vargas <pedrovargas.cba@gmail.com>
2026-05-19 11:31:17 -0400

    fix: músicas alteradas para formato .ogg
    *

--- Scripts/Cutscene/cutscene_resource.gd
+++ Scripts/Cutscene/cutscene_resource.gd
@@ -7,7 +7,7 @@ class_name CutsceneResource
 @export var usar_fade_in_audio: bool = true
 @export var delay_inicial: float = 1.0
 @export var tempo_fade_inicial: float = 1.0
-@export var tempo_auto_avanco: float = 4.0
+@export var tempo_auto_avanco: float = 6.0
 
 @export_group("Páginas")
 @export var paginas: Array[CutscenePage] = []
